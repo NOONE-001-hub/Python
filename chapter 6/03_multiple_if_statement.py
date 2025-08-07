c02decd40930d89c7accfc341fccc73fdb22c80f
NOONE-001-hub <snehit.test@gmail.com>
2025-08-07 22:56:02 +0530

    done

---
+++ chapter 6/03_multiple_if_statement.py
@@ -0,0 +1,21 @@
+a = int(input("Enter Your age : "))
+
+# If statement no 1
+if(a%2 == 0):
+     print("a is even")
+
+# End of If statemnt no 1
+
+# If statement no:2 
+if(a>=18):
+    print("You are above the age of consent")
+    print("Good for you")
+
+elif(a<0):
+    print("You are entering an invalid neagtive age")
+
+else:
+    print("You are below the age of consent")
+# end of If statemnet no 2
+
+print("End of Program")   
