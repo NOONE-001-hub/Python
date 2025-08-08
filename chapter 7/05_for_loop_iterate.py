fe6c6604fac90a8a99a4eaf8bf5ddcc9c596bcfe
NOONE-001-hub <snehit.test@gmail.com>
2025-08-08 10:03:01 +0530

    done

---
+++ chapter 7/05_for_loop_iterate.py
@@ -0,0 +1,14 @@
+# for loop with Lists
+l = [1, 4, 5, 844, 33, 2334] 
+for i in l:
+    print(i)
+
+# For loop with Tuples
+t = (1, 4, 34, 35,) 
+for i in t:
+    print(i)
+
+#For Loop with Strings
+s = "Kanna"
+for i in s:
+    print(i)
