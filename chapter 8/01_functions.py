786aa9faf6f04da549b583c6539109971c1958b6
NOONE-001-hub <snehit.test@gmail.com>
2025-08-08 15:44:03 +0530

    done

---
+++ chapter 8/01_functions.py
@@ -0,0 +1,33 @@
+# a = int(input("Enter Your number: "))
+# b = int(input("Enter Your number: "))
+# c = int(input("Enter Your number: "))
+
+# average = (a+b+c)/3
+# print(average)
+
+
+# a = int(input("Enter Your number: "))
+# b = int(input("Enter Your number: "))
+# c = int(input("Enter Your number: "))
+
+# average = (a+b+c)/3
+# print(average)
+
+
+# Function Defination
+def avg():
+    a = int(input("Enter Your number: "))
+    b = int(input("Enter Your number: "))
+    c = int(input("Enter Your number: "))
+
+    average = (a+b+c)/3
+    print(average)
+
+avg() # Function call
+print("Thank You!")
+avg()
+print("Thank You!")
+avg()
+print("Thank You!")
+avg()
+avg()
