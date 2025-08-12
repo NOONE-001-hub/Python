16627dcdacb59e9b97c556f52b48e191b9c9fa07
NOONE-001-hub <snehit.test@gmail.com>
2025-08-12 14:04:55 +0530

    object oriented Programming

---
+++ chapter 10 ps/01_problem1.py
@@ -0,0 +1,9 @@
+class Programmer:
+    company = "Microsoft"
+    def __init__(self, name , salary, pin):
+       self.name = name
+       self.salary = salary
+       self.pin = pin
+
+p = Programmer("Kanna", 1300000, 4521) 
+print(p.name, p.salary, p.pin, p.company)
